2ddbdd0fff9a91a4aa5f2b7e4497c61d6d313ce1
Chaudaryabdullah89 <chaudaryabdullah899@gmail.com>
2025-05-21 13:21:01 +0500

    aesf

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://apna-backend.vercel.app/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://apna-backend.vercel.app';
 
 const instance = axios.create({
   baseURL: API_URL,

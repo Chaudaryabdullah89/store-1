3d0a2e59c4fb54c1bf0f2248f03db55aa74a1821
Chaudaryabdullah89 <chaudaryabdullah899@gmail.com>
2025-05-21 08:52:32 +0500

    123

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -6,6 +6,9 @@ const instance = axios.create({
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
   }
 });
 
